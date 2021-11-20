3113ed497d13ef072ea1bc02bb7559a17d685e7d
誠 <87258462+Taisei-dev@users.noreply.github.com>
2021-11-20 11:23:47 +0900

    update main.js

--- main.js
+++ main.js
@@ -2,26 +2,13 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-  if(req.url==='/'){
-    fs.readFile(__dirname + '/index.html',(err,data)=>{
-      if (err) {
-        res.writeHead(404);
-        res.end(JSON.stringify(err));
-        return;
-      }
-      res.writeHead(200);
-      res.end(data);
-    })
-  }
-  else{
-    fs.readFile(__dirname + req.url, function (err,data) {
-      if (err) {
-        res.writeHead(404);
-        res.end(JSON.stringify(err));
-        return;
-      }
-      res.writeHead(200);
-      res.end(data);
-    });
-  }
+  fs.readFile(__dirname + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
 }).listen(process.env.PORT);
