8799e8902d300413c6dd9ada61a81958ca9cee59
誠 <87258462+Taisei-dev@users.noreply.github.com>
2021-11-20 11:13:17 +0900

    update two files

--- main.js
+++ main.js
@@ -2,13 +2,26 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-  fs.readFile(__dirname + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
+  if(req.url='/'){
+    fs.readFile(__dirname + '/index.html',(err,data)=>{
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    })
+  }
+  else{
+    fs.readFile(__dirname + req.url, function (err,data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
 }).listen(process.env.PORT);
